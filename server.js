081cdbf5579bf6310218ca3efc96667bf547752b
novinary <novina.dev@gmail.com>
2019-04-19 10:30:39 +0100

    Write code (route handlers)

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const games = require('./data/gamesModel');
+const express = require("express");
+const games = require("./data/gamesModel");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', async (req, res) => {
-	res.status(200).json({ api: 'up' });
+server.get("/", async (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.get("/games", async (req, res) => {
+  const rows = await games.getAll();
+  res.status(200).json(rows);
+});
+
+server.post("/games", (req, res) => {
+  const game = req.body;
+  if (!game.title || !game.genre) {
+    res.status(422).json({ message: "Some info is missing!" });
+  } else {
+    games.insert(game);
+    res.status(201).json({ message: "Created" });
+  }
 });
 
 module.exports = server;
